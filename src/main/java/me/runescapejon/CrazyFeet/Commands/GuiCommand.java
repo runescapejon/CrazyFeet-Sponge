261323f688abcf5276f1c51d8dce3bb1719e87dd
runescapejon <jonsimpletv@gmail.com>
2017-12-13 13:37:36 -0500

    Oops i made a derp

--- src/main/java/me/runescapejon/CrazyFeet/Commands/GuiCommand.java
+++ src/main/java/me/runescapejon/CrazyFeet/Commands/GuiCommand.java
@@ -128,7 +128,7 @@ public class GuiCommand implements CommandExecutor {
 			if (item.getItem().equals(ItemTypes.TNT)) {
 				player.playSound(SoundTypes.ENTITY_PLAYER_LEVELUP, player.getLocation().getPosition(), 1);
 				Sponge.getScheduler().createTaskBuilder().delayTicks(1).
-						execute(() -> Sponge.getCommandManager().process(player, "crazysmoke")).
+						execute(() -> Sponge.getCommandManager().process(player, "crazysmokehead")).
 						submit(CrazyFeet.getInstance());
 			}
 			if (item.getItem().equals(ItemTypes.BARRIER)) {
